f7c580d52314da91b082167d56a35e965d80960e
yukatz <yukatz578@gmail.com>
2022-01-08 13:41:12 +0200

    Tests for all classes

---
+++ Test/Load_Test.py
@@ -0,0 +1,22 @@
+import unittest
+from load_data_from_server import *
+import networkx as nx
+
+
+class MyTestCase(unittest.TestCase):
+    def load_graph_json_test(self):
+        self.assertEqual()
+
+    def load_agents_list_test(self):
+        self.assertEqual()
+
+    def load_pokemon_list_test(self):
+        self.assertEqual()
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
